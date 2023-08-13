fb275df633a7976a2de64cd6696d0918feb2853d
wloodar <wlodardev@gmail.com>
2023-08-13 18:54:52 +0200

    Client socket connection to the server

--- src/client.c
+++ src/client.c
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
+#include "common.h"
 #include "xcp.h"
 #include "client.h"
 
@@ -14,10 +18,27 @@ char config_path_username[64];
 
 int main(int argc, char **argv)
 {  
+    // Setup crucial parts of client
     initialize();
 
     char* username = get_username();
-    printf(username);
+    
+    // Setup socket for client <-> server communication
+    struct sockaddr_in addr;
+    int sock;
+
+    if (argc < 2) {
+        die("Missing argument: <host>");
+    }
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(XCP_SEVER_PORT);
+
+    if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
+        die("Couldn't connect to the xcloud server.");
+    }
 
     return 0;
 }
